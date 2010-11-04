983a30e792eed53a6b2e065f6cef1f88dcafdb1b
Ravi Chandra Padmala <neena@theworld.strangled.net>
2010-11-04 03:35:47 +0300

    navlist..

--- search-theme-form.tpl.php
+++ search-theme-form.tpl.php
@@ -36,7 +36,7 @@
 <div id="search" class="container-inline">
 <?php
   $search["search_theme_form"]= str_replace("Search this site: ", "", $search["search_theme_form"]);
-  $search["submit"] = str_replace("<input", "<input style=\"visibility: hidden\";", $search["submit"]);
+  $search["submit"] = str_replace("<input", "<input style=\"display: none\";", $search["submit"]);
   print $search["search_theme_form"];
   print $search["submit"];
   print $search["hidden"];
